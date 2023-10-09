988e453c6e8199cbaf43e34ff7f02831021c58ac
i18n <i18n.site@gmail.com>
2023-10-09 16:44:48 +0800

    @8p/full v0.0.1

---
+++ full/src/Full.svelte
@@ -0,0 +1,32 @@
+<script lang="coffee">
+
++ f
+
+nowFull = =>
+  f = document.fullscreenElement
+  return
+
+click = =>
+  if document.fullscreenElement
+    p = document.exitFullscreen()
+  else
+    p = document.body.requestFullscreen navigationUI:'hide'
+  try
+    await p
+  finally
+    nowFull()
+  return
+</script>
+
+<template lang="pug">
+m-c(@click class:f=f title:>full)
+</template>
+
+<style lang="stylus">
+m-c
+  background-image var(--svgFull)
+
+  &.f
+    background-image var(--svgUnfull)
+</style>
+
